cd7b2e91cc4bc24869ac37748a0a06353013667a
mrwindupbirdesq <nmaahs11@gmail.com>
2024-04-18 10:25:33 -0700

    Create mock API to fetch CSV files

---
+++ src/app/csvAPI.js
@@ -0,0 +1,15 @@
+import fs from 'fs';
+import path from 'path';
+
+export default function handler(req, res) {
+    const directoryPath = path.join(process.cwd(), 'public/CSV-Files');
+    
+    fs.readdir(directoryPath, (err, files) => {
+        if (err) {
+            return res.status(500).json({ message: "Unable to scan files!" });
+        }
+
+        const csvFiles = files.filter(file => file.endsWith('.csv'));
+        res.status(200).json({ files: csvFiles });
+    });
+}
