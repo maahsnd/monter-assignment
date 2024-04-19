97e0f07cf0069561a48bf9dfb0a2e8f744f4fc36
mrwindupbirdesq <nmaahs11@gmail.com>
2024-04-19 13:19:32 -0700

    Implement filter on back end

--- src/pages/api/csv-api.js
+++ src/pages/api/csv-api.js
@@ -1,9 +1,13 @@
 import fs from 'fs';
 import path from 'path';
+import url from 'url';
 
 export default function handler(req, res) {
   const directoryPath = path.join(process.cwd(), 'public/CSV-Files');
   const metadataPath = path.join(process.cwd(), 'public/metadata.json');
+
+  const reqUrl = url.parse(req.url, true);
+  const { startDate, endDate, sortByDate } = reqUrl.query;
 
   fs.readFile(metadataPath, 'utf8', (err, metadataContent) => {
     if (err) {
@@ -25,14 +29,35 @@ export default function handler(req, res) {
         return res.status(500).json({ message: 'Unable to scan files!' });
       }
 
-      const csvFiles = files.filter((file) => file.endsWith('.csv'));
+      let filesWithDates = files
+        .filter((file) => file.endsWith('.csv'))
+        .map((file) => ({
+          file,
+          creationDate: metadata[file]
+            ? new Date(metadata[file].creationDate)
+            : null
+        }));
 
-      const filesWithDates = csvFiles.map((file) => ({
-        file: file,
-        creationDate: metadata[file].creationDate || 'Unknown'
-      }));
+      // Filtering by startDate and/or endDate
+      if (startDate || endDate) {
+        const startDateObj = startDate
+          ? new Date(startDate)
+          : new Date(-8640000000000000); // Use earliest possible date if no start date
+        const endDateObj = endDate ? new Date(endDate) : new Date(); // Use current date if no end date
 
-      filesWithDates.sort((a, b) => b.creationDate - a.creationDate);
+        filesWithDates = filesWithDates.filter(
+          ({ creationDate }) =>
+            creationDate &&
+            creationDate >= startDateObj &&
+            creationDate <= endDateObj
+        );
+      }
+      // Sorting logic
+      if (sortByDate === 'asc') {
+        filesWithDates.sort((a, b) => a.creationDate - b.creationDate);
+      } else {
+        filesWithDates.sort((a, b) => b.creationDate - a.creationDate);
+      }
 
       res.status(200).json({ files: filesWithDates.slice(0, 30) });
     });
