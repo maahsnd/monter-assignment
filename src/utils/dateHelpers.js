212aad83b68fa60d27b09a91ea3d543eda52c862
mrwindupbirdesq <nmaahs11@gmail.com>
2024-04-19 13:17:28 -0700

    Force local time April 1 was being converted to March 31.

--- src/utils/dateHelpers.js
+++ src/utils/dateHelpers.js
@@ -1,12 +1,18 @@
-// Function to format dates as "day.month.year"
+// Function to format dates as "day.month.year", forcing local time
 export function formatDate(dateString) {
-  const date = new Date(dateString);
-  const day = date.getDate();
-  const month = date.getMonth() + 1; // getMonth() is zero-based
-  const year = date.getFullYear();
-  return `${day < 10 ? '0' + day : day}.${
-    month < 10 ? '0' + month : month
-  }.${year}`;
+  const parts = dateString.split('-'); // Split the date string into components
+  const year = parseInt(parts[0], 10);
+  const month = parseInt(parts[1], 10) - 1; // Adjust month to be zero-based
+  const day = parseInt(parts[2], 10);
+
+  const date = new Date(year, month, day); // This uses local time zone
+  const formattedDay = date.getDate();
+  const formattedMonth = date.getMonth() + 1; // getMonth() is zero-based
+  const formattedYear = date.getFullYear();
+
+  return `${formattedDay < 10 ? '0' + formattedDay : formattedDay}.${
+    formattedMonth < 10 ? '0' + formattedMonth : formattedMonth
+  }.${formattedYear}`;
 }
 
 // Function to format time as "6:00 PM" or "6:00 AM"
